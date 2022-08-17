9e62e39879c8362a9ab92d2d925bb1be8a8354e3
Justin Spegele <justin.spegele@gmail.com>
2022-08-17 17:42:06 -0400

    Moved common sign in/up elements up to layout

--- src/components/Layouts/SignUpLayout.component.js
+++ src/components/Layouts/SignUpLayout.component.js
@@ -1,10 +1,21 @@
 import React from "react"
 import { Outlet } from "react-router-dom"
 
+import logo from "../../assets/images/icon-green.png"
+
 const SignUpLayout = () => {
   return (
     <div className="flex items-center justify-center min-h-screen min-w-[280px] bg-emerald-800">
-      <Outlet />
+      <div className="w-64 sm:w-96 bg-white px-8 py-12 rounded">
+        <div className="flex direction-row items-center">
+          <img src={logo} className="w-5" alt="" />
+          <span className="text-4xl font-semibold pl-2">Expensify</span>
+        </div>
+        <div className="mt-2 text-semibold text-emerald-800">Take control of your spending</div>
+        <div className="mt-8">
+          <Outlet />
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/SignUpSignIn/ForgotPasswordPage.component.js
+++ src/components/SignUpSignIn/ForgotPasswordPage.component.js
@@ -8,16 +8,11 @@ const ForgotPasswordPage = () => {
   const [sent, setSent] = useState(false)
 
   return (
-    <div className="w-64 sm:w-96 bg-white px-8 py-12 rounded">
-      <div className="flex direction-row items-center">
-        <img src={logo} className="w-5" alt="" />
-        <span className="text-4xl font-semibold pl-2">Expensify</span>
-      </div>
-      <div className="mt-8">Take control of your spending</div>
+    <div>
       {sent ? (
-        <div className="mt-16">Success! Check your email for a password reset link.</div>
+        <div className="mt-8">Success! Check your email for a password reset link.</div>
       ) : (
-        <div className="mt-8">
+        <div>
           <ForgotPasswordForm setSent={setSent} />
         </div>
       )}
