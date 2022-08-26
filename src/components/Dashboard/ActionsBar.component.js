aa38acd24af5aa9ba81003c822797054a4598abf
Justin Spegele <justin.spegele@gmail.com>
2022-08-26 12:28:06 -0400

    Renamed button

--- src/components/Dashboard/ActionsBar.component.js
+++ src/components/Dashboard/ActionsBar.component.js
@@ -6,7 +6,7 @@ const ActionsBar = () => {
   return (
     <div className="flex items-end justify-end mt-8">
       <Link className="px-4 py-2 text-white bg-sky-700 hover:bg-sky-600 active:bg-sky-700 font-semibold rounded transition-colors" to="/create">
-        Create Expense
+        + Transaction
       </Link>
     </div>
   )
