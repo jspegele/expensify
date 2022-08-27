283f96a2541527a3972c4fb84bcd7648085a64af
Justin Spegele <justin.spegele@gmail.com>
2022-08-26 20:14:43 -0400

    Set default start and end date to current month

--- src/contexts/FiltersContext.js
+++ src/contexts/FiltersContext.js
@@ -2,10 +2,33 @@ import React, { useState, createContext } from "react"
 
 export const FiltersContext = createContext()
 
+const getCurrentYear = () => {
+  const date = new Date()
+  return date.getFullYear()
+}
+
+const getCurrentMonth = () => {
+  const date = new Date()
+  return (date.getMonth() + 1).toString().padStart(2, '0')
+}
+
+const isLeapYear = (year) => {
+  if (((year % 4 === 0) && (year % 100 !== 0)) || (year % 400 === 0) ) return true
+  return false
+}
+
+const getLastDayOfCurrentMonth = () => {
+  const daysInFeb = isLeapYear(getCurrentYear()) ? '29' : '28'
+  const daysInMonth = ['31', daysInFeb,'31','30','31','30','31','31','30','31','30','31']
+  console.log(daysInMonth)
+  const date = new Date()
+  return daysInMonth[date.getMonth()]
+}
+
 const initialState = {
   text: "",
-  startDate: "",
-  endDate: "",
+  startDate: `${getCurrentYear()}-${getCurrentMonth()}-01`,
+  endDate: `${getCurrentYear()}-${getCurrentMonth()}-${getLastDayOfCurrentMonth()}`,
   sortBy: "dateDesc",
 }
 
