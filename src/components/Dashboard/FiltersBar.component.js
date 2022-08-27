b828bc6882014a9b66cfcf0fc133efabacb9c180
Justin Spegele <justin.spegele@gmail.com>
2022-08-26 20:17:29 -0400

    Added responsiveness to FiltersBar

--- src/components/Dashboard/FiltersBar.component.js
+++ src/components/Dashboard/FiltersBar.component.js
@@ -5,11 +5,11 @@ import DateFilter from "./DateFilter.component"
 import SortSelect from "./SortSelect.component"
 
 const FiltersBar = () => (
-  <div className="flex justify-between -mx-2">
-    <div className="grow px-2">
+  <div className="flex flex-col md:flex-row justify-between -mx-2">
+    <div className="grow px-2 mb-2 md:mb-0">
       <SearchBar />
     </div>
-    <div className="px-2">
+    <div className="px-2 mb-2 md:mb-0">
       <DateFilter />
     </div>
     <div className="px-2">
